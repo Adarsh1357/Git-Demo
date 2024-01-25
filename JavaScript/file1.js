693399a1391b7d3a4f4e7f2615f9871944e460fc
Adarsh <adarsh975315@gmail.com>
2024-01-25 16:45:22 +0530

    make some changes in file

--- JavaScript/file1.js
+++ JavaScript/file1.js
@@ -1,1 +1,7 @@
 console.log("hello World");
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.indexOf(3));
+console.log(Array.isArray(arr));
+console.log(arr.includes(5));
+console.log(arr.includes(9)); 
