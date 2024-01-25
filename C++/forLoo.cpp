cabe6d91ddebaef497b0862e83b417d5150d8af4
Adarsh <adarsh975315@gmail.com>
2024-01-25 16:25:30 +0530

    initial commit

---
+++ C++/forLoo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // for loop
+  for(int i=0; i<5; i++){
+    for(int j=0; j<5; j++){
+      cout<<"* ";
+    }
+    cout<<endl;
+  }
+}
---
+++ JavaScript/file2.js
@@ -0,0 +1,3 @@
+console.log("I am learning git");
+console.log("23" > 22);          // true
+console.log(49 + 44);
